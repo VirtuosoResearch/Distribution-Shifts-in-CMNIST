09fbdfe80d8d4eb25b40ec84f5767fa8048e1816
Kinshuk <kinshuk1207@gmail.com>
2022-06-27 12:12:26 -0400

    Initial commit

---
+++ datasets.py
@@ -0,0 +1,87 @@
+import torch
+from PIL import ImageFile
+from torch.utils.data import TensorDataset
+from torchvision.datasets import MNIST
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+class MultipleDomainDataset:
+    N_STEPS = 5001
+    CHECKPOINT_FREQ = 100
+    N_WORKERS = 8
+    ENVIRONMENTS = None
+    INPUT_SHAPE = None
+
+    def __getitem__(self, index):
+        return self.datasets[index]
+
+    def __len__(self):
+        return len(self.datasets)
+
+
+class MultipleEnvironmentMNIST(MultipleDomainDataset):
+    def __init__(self, root, environments, dataset_transform, input_shape,
+                 num_classes):
+        super().__init__()
+        if root is None:
+            raise ValueError('Data directory not specified!')
+
+        original_dataset_tr = MNIST(root, train=True, download=True)
+        original_dataset_te = MNIST(root, train=False, download=True)
+
+        original_images = torch.cat((original_dataset_tr.data,
+                                     original_dataset_te.data))
+
+        original_labels = torch.cat((original_dataset_tr.targets,
+                                     original_dataset_te.targets))
+
+        shuffle = torch.randperm(len(original_images))
+
+        original_images = original_images[shuffle]
+        original_labels = original_labels[shuffle]
+
+        self.datasets = []
+
+        for i in range(len(environments)):
+            images = original_images[i::len(environments)]
+            labels = original_labels[i::len(environments)]
+            self.datasets.append(dataset_transform(images, labels, environments[i]))
+
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+
+
+class ColoredMNIST(MultipleEnvironmentMNIST):
+    ENVIRONMENTS = ['+90%', '+80%', '-90%']
+
+    def __init__(self, root, test_envs, hparams):
+        super(ColoredMNIST, self).__init__(root, [0.1, 0.2, 0.9],
+                                           self.color_dataset, (2, 28, 28,), 2)
+
+        self.input_shape = (2, 28, 28,)
+        self.num_classes = 2
+
+    def color_dataset(self, images, labels, environment):
+        labels = (labels < 5).float()
+        labels = self.torch_xor_(labels,
+                                 self.torch_bernoulli_(0.25, len(labels)))
+
+        colors = self.torch_xor_(labels,
+                                 self.torch_bernoulli_(environment,
+                                                       len(labels)))
+        images = torch.stack([images, images], dim=1)
+
+        images[torch.tensor(range(len(images))), (
+                                                         1 - colors).long(), :, :] *= 0
+
+        x = images.float().div_(255.0)
+        y = labels.view(-1).long()
+
+        return TensorDataset(x, y)
+
+    def torch_bernoulli_(self, p, size):
+        return (torch.rand(size) < p).float()
+
+    def torch_xor_(self, a, b):
+        return (a - b).abs()
